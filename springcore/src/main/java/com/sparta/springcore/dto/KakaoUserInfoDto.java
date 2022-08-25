d0024c77d27e3c04579832aea25368fa73e1256d
minseolki <110362442+minseolki@users.noreply.github.com>
2022-08-25 14:19:31 +0900

    Add files via upload

---
+++ springcore/src/main/java/com/sparta/springcore/dto/KakaoUserInfoDto.java
@@ -0,0 +1,11 @@
+package com.sparta.springcore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+@Getter
+@AllArgsConstructor
+public class KakaoUserInfoDto {
+    private Long id;
+    private String nickname;
+    private String email;
+}
